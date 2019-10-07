57d6ce7d5574dd8898e31a83bf2aecd8b5b92cb5
Theradesh Chaiyapromma <tc@manaosoftware.com>
2019-10-07 13:03:20 +0700

    Add assignmentDB

---
+++ assignment2C2P/assignment2C2P/Infrastructure/EF/AssignmentDB.cs
@@ -0,0 +1,28 @@
+﻿using assignment2C2P.Infrastructure.EF.Mapping;
+using assignment2C2P.Models.Customers;
+using assignment2C2P.Models.Transactions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2C2P.Infrastructure.EF
+{
+    public class AssignmentDB : DbContext
+    {
+        public AssignmentDB(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.CustomerMap();
+            modelBuilder.TransactionMap();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
