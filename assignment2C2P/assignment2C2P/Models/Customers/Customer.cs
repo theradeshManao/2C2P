0061c0514c4afa030bb00e6227c2811e93cee781
Theradesh Chaiyapromma <tc@manaosoftware.com>
2019-10-07 11:36:49 +0700

    add class customer and transaction

---
+++ assignment2C2P/assignment2C2P/Models/Customers/Customer.cs
@@ -0,0 +1,17 @@
+﻿using assignment2C2P.Models.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2C2P.Models.Customers
+{
+    public class Customer
+    {
+        public long CustomerID { get; set; } // Numberic max 10 chars
+        public string Name { get; set; } // Character max 30 chars
+        public string Email { get; set; } //Character max 25 digits
+        public string MobileNumber { get; set; } // Numeric only 10 digits
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>(); // max 5 records
+    }
+}
---
+++ assignment2C2P/assignment2C2P/Models/Transactions/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2C2P.Models.Transactions
+{
+    public class Transaction
+    {
+        public long TransactionID { get; set; } // Numeric dont have max limit
+        public DateTime DateTime { get; set; } // DD/MM/YY HH:MM e.g. 31/02/2018 21:34
+        public double Amount { get; set; } // 2 decimal points
+        public string CurrencyCode { get; set; } // have only 3 characters
+        //public TransactionStatus TransactionStatus { get; set; } // have 3 formats
+    }
+}
