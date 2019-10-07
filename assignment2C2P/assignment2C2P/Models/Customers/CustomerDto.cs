a0a490495b81d778183cfc30302192d370b6962f
Theradesh Chaiyapromma <tc@manaosoftware.com>
2019-10-07 16:53:06 +0700

    Added CustomerDto

---
+++ assignment2C2P/assignment2C2P/Models/Customers/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using assignment2C2P.Models.Transactions;
+using System.Collections.Generic;
+
+namespace assignment2C2P.Models.Customers
+{
+    public class CustomerDto
+    {
+        public long CustomerId { get; set; } // Numberic max 10 chars
+        public string Name { get; set; } // Character max 30 chars
+        public string Email { get; set; } //Character max 25 digits
+        public string MobileNumber { get; set; } // Numeric only 10 digits
+        public List<TransactionDto> Transactions { get; set; } = new List<TransactionDto>(); // max 5 records
+    }
+}
