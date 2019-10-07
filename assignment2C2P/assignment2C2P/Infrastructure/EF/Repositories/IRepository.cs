20f36edaa7269da6804f19633560d4e6f94f1887
Theradesh Chaiyapromma <tc@manaosoftware.com>
2019-10-07 15:40:09 +0700

    Add GetByCustomerId method in Repository

---
+++ assignment2C2P/assignment2C2P/Infrastructure/EF/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using assignment2C2P.Models.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2C2P.Infrastructure.EF.Repositories
+{
+    public interface IRepository
+    {
+        Task<Customer> GetByCustomerId(int customerId);
+    }
+}
---
+++ assignment2C2P/assignment2C2P/Infrastructure/EF/Repositories/Repository.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Threading.Tasks;
+using assignment2C2P.Infrastructure.EF.Repositories;
+using assignment2C2P.Models.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignment2C2P.Infrastructure.EF.Repositories
+{
+    public class Repository : IRepository
+    {
+        public AssignmentDB context { get; set; }
+
+        public Repository(AssignmentDB context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Customer> GetByCustomerId(int customerId)
+        {
+            var customer = await context.Customers
+                .SingleOrDefaultAsync(x => x.CustomerId == customerId);
+            return customer;
+        }
+    }
+}
